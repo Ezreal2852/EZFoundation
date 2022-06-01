3d44ea1eafdcbb1170a9a423f34d1a436797f197
liujiahao <jiahao.office@foxmail.com>
2022-06-01 12:31:15 +0800

    添加：demo；

---
+++ Demo/Demo/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by 刘嘉豪 on 2022/6/1.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 刘嘉豪 on 2022/6/1.
+//
+
+import UIKit
+import EZFoundation
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
