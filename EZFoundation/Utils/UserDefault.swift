b35717a31cdddbbef65dff364778abcca13fe1dc
liujiahao <jiahao.office@foxmail.com>
2023-02-06 11:57:22 +0800

    添加：UserDefault基于@propertyWrapper使用第三方的实践示例；

--- EZFoundation/Utils/UserDefault.swift
+++ EZFoundation/Utils/UserDefault.swift
@@ -29,3 +29,50 @@ public struct UserDefault<T> {
     /// - Parameter key: String key
     public init(_ key: String) { self.key = key }
 }
+
+//MARK: - 基于@propertyWrapper使用第三方的实践
+
+#if canImport(DefaultsKit)
+
+/// pod 'DefaultsKit', '~> 0.2.0' # https://github.com/nmdias/DefaultsKit
+@_exported import DefaultsKit
+
+//MARK: 统一定义 UserDefaults.keys
+
+extension DefaultsKey {
+    /// 测试用，仅做演示
+    static let testKey = Key<String>("testKey")
+}
+
+//MARK: 包装器，作为var使用
+
+@propertyWrapper
+public struct DefaultsVar<T: Codable> {
+    
+    public let key: Key<T>
+    
+    public var wrappedValue:T? {
+        get { Defaults.shared.get(for: key) }
+        set { newValue == nil ? Defaults.shared.clear(key) : Defaults.shared.set(newValue!, for: key) }
+    }
+    
+    public init(_ key: Key<T>) { self.key = key }
+}
+
+//MARK: ⬇️演示使用代码
+
+fileprivate func testDefaultsKey() {
+    // 直接使用
+    Defaults.shared.set("test value", for: .testKey)
+    _ = Defaults.shared.get(for: .testKey)
+    
+    // 作为成员变量使用
+    TestDefaultsVar.testValue = "testValue"
+    _ = TestDefaultsVar.testValue
+}
+
+fileprivate struct TestDefaultsVar {
+    @DefaultsVar(.testKey) static var testValue
+}
+
+#endif
