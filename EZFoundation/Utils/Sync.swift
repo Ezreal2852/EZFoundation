f76d2a6ce0134c8bea6860bc322e0af38e06e1a5
liujiahao <jiahao.office@foxmail.com>
2023-02-06 12:03:03 +0800

    完善：synchronized 的注释，便于理解obj的用意；

--- EZFoundation/Utils/Sync.swift
+++ EZFoundation/Utils/Sync.swift
@@ -7,10 +7,10 @@
 
 import ObjectiveC
 
-/// 递归锁，同OC-@synchronized
+/// 递归互斥锁，同OC-@synchronized
 /// - Parameters:
-///   - obj: 被锁定的对象
-///   - `do`: 执行安全访问操作
+///   - obj: 绑定锁的对象，但不一定需要访问obj，但需标识，用于创建锁和访问锁。
+///   - `do`:  执行安全访问操作
 public func synchronized<T>(_ obj: T, `do`: (_ obj: T) -> Void) {
     objc_sync_enter(obj)
     `do`(obj)
