94a3cc651d976fafc482b4939f8cadb9f2dd4eb1
liujiahao <jiahao.office@foxmail.com>
2023-02-17 18:46:20 +0800

    添加：一个内存泄漏的场景；

--- EZFoundationDemo/EZFoundationDemo/ViewController.swift
+++ EZFoundationDemo/EZFoundationDemo/ViewController.swift
@@ -11,10 +11,29 @@ import EZFoundation
 class ViewController: UIViewController {
     
     @UserDefault("kMyInt") var myInt: Int?
+    
+    fileprivate lazy var leakView = TestLeakView(frame: .zero)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        leakView.tap = handleLeak // ❌
+        
+        leakView.tap = { [weak self] in // ✅
+            self?.handleLeak()
+        }
     }
+    
+    fileprivate func handleLeak() {
+        weak var `self` = self // 哪怕这样写，在{}作用域内，等号右边的self仍会被强引用
+        guard let self = self else { return }
+        print(self, "此时，这里的self都会被leakView.tap强引用，从而产生循环引用！")
+    }
+}
+
+fileprivate class TestLeakView: UIView {
+    
+    // ⚠️需要谨慎把func当closure来使用！
+    var tap: (() -> Void)?
 }
 
