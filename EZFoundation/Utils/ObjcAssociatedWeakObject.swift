36487506703fe407d4afdb24b80937eee9697635
liujiahao <jiahao.office@foxmail.com>
2024-01-05 11:28:34 +0800

    完善：ObjcAssociatedWeakObject实现，并添加使用示例；

--- EZFoundation/Utils/ObjcAssociatedWeakObject.swift
+++ EZFoundation/Utils/ObjcAssociatedWeakObject.swift
@@ -1,28 +1,36 @@
 //
 //  ObjcAssociatedWeakObject.swift
-//  AssociatedObject 弱引用一个对象
+//  AssociatedObject 关联弱引用对象，唯一，且读写类型一致。
 
 import ObjectiveC
 
-public func objc_getAssociatedWeakObject(_ object: AnyObject, _ key: UnsafeRawPointer) -> AnyObject? {
-    let block: (() -> AnyObject?)? = objc_getAssociatedObject(object, key) as? (() -> AnyObject?)
-    return block != nil ? block?() : nil
+fileprivate typealias AssociatedObjectBlock = (() -> AnyObject?)
+
+fileprivate func objc_getAssociatedWeakObject(_ object: AnyObject, _ key: UnsafeRawPointer) -> AnyObject? {
+    let block = objc_getAssociatedObject(object, key) as? AssociatedObjectBlock
+    return block?()
 }
 
-public func objc_setAssociatedWeakObject(_ object: AnyObject, _ key: UnsafeRawPointer, _ value: AnyObject?) {
+fileprivate func objc_setAssociatedWeakObject(_ object: AnyObject, _ key: UnsafeRawPointer, _ value: AnyObject?) {
     weak var weakValue = value
-    let block: (() -> AnyObject?)? = {
-        return weakValue
-    }
+    let block: AssociatedObjectBlock? = { weakValue }
     objc_setAssociatedObject(object, key, block, .OBJC_ASSOCIATION_COPY)
 }
 
 fileprivate let kWeakObj = malloc(4)! // UnsafeMutableRawPointer!
 
-fileprivate extension NSObject {
+extension NSObject {
     
-    var weakObj: AnyObject? {
+    fileprivate var weakObject: AnyObject? {
         set { objc_setAssociatedWeakObject(self, kWeakObj, newValue) }
         get { objc_getAssociatedWeakObject(self, kWeakObj) }
+    }
+    
+    public func setWeakObject<T: AnyObject>(_ object: T?) {
+        weakObject = object
+    }
+    
+    public func getWeakObject<T: AnyObject>() -> T? {
+        weakObject as? T
     }
 }
--- EZFoundationDemo/EZFoundationDemo/ViewController.swift
+++ EZFoundationDemo/EZFoundationDemo/ViewController.swift
@@ -24,16 +24,44 @@ class ViewController: UIViewController {
         }
     }
     
-    fileprivate func handleLeak() {
+    fileprivate func handleLeak() { // [weak self] in 这样写也会报错：Cannot find 'weak' in scope
         weak var `self` = self // 哪怕这样写，在{}作用域内，等号右边的self仍会被强引用
         guard let self = self else { return }
         print(self, "此时，这里的self都会被leakView.tap强引用，从而产生循环引用！")
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        for touch in touches {
+            if touch.view == view {
+                present(WeakViewController(), animated: true)
+                break
+            }
+        }
+    }
 }
 
 fileprivate class TestLeakView: UIView {
-    
     // ⚠️需要谨慎把func当closure来使用！
     var tap: (() -> Void)?
+}
+
+
+class WeakViewController: UIViewController {
+    
+    deinit {
+        print(#function, self)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setWeakObject(self)
+        
+        if let weakSelf: WeakViewController = getWeakObject() {
+            print(#function, weakSelf)
+        }
+    }
 }
 
